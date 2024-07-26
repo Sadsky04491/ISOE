bcd1edc6999b0d403e4744d4048015a9f336dc3a
Sadsky04491 <92867780+Sadsky04491@users.noreply.github.com>
2024-07-26 19:47:50 -0300

    Create Script.js

---
+++ kubejs/startup_scripts/Script.js
@@ -0,0 +1,17 @@
+Platform.mods.kubejs.name = 'Eternity',
+Platform.mods.bigreactors.name = 'Big Reactors / Extreme Reactors',
+
+StartupEvents.registry('item', event => {
+  event.create('estelar_obsidium_ingot').displayName('Estelar Obsidium Ingot'),
+  event.create('estelar_obsidium_nugget').displayName('Estelar Obsidium Nugget'),
+  event.create('eternity_core').displayName('Eternity Core'),
+  event.create('eternity_ingot').displayName('Eternity Ingot'),
+  event.create('eternity_shaper').displayName('Eternity Shaper'),
+  event.create('awakened_draconium_plate').displayName('Awakened Draconium Plate'),
+  event.create('crystal_matrix_plate').displayName('Crystal Matrix Plate'),
+  event.create('dark_matter_plate').displayName('Dark Matter Plate'),
+  event.create('draconium_plate').displayName('Draconium Plate'),
+  event.create('neutronium_plate').displayName('Neutronium Plate'),
+  event.create('red_matter_plate').displayName('Red Matter Plate'),
+  event.create('infinity_plate').displayName('Infinity Plate')
+})
